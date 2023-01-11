a31bad5e4ecdf23160e44bb659434ae09f1ed2c4
YOUALEU Frank Loic <frankpythagore@gmail.com>
2023-01-11 19:49:53 +0100

    route

--- src/main/java/com/example/nexgel/Controller/UserController.java
+++ src/main/java/com/example/nexgel/Controller/UserController.java
@@ -30,7 +30,7 @@ import com.example.nexgel.model.User;
 import lombok.AllArgsConstructor;
 
 @RestController
-@RequestMapping(path = "api/register")
+@RequestMapping(path = "api")
 @AllArgsConstructor
 public class UserController {
 
@@ -43,12 +43,12 @@ public class UserController {
     
     private final UserRepository userRepository;
     
-    @PostMapping()
+    @PostMapping("/register")
     public String register (@RequestBody RegistrationRequest request){
         return registrationService.register(request);
     }
 
-    @PostMapping(path = "login")
+    @PostMapping("/login")
 	public ResponseEntity<?> login(@RequestBody RequestAuthentication authenticationRequest) throws InvalidKeySpecException, NoSuchAlgorithmException {
 
 		final Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
